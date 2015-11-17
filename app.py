d5027100c32322fb6d1ef6a8a357b9fa9d5691ce
agentreno <kturrell@hotmail.com>
2015-11-17 21:30:37 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   firstname = db.Column(db.String(30))
+   lastname = db.Column(db.String(40))
+   dateofbirth = db.Column(db.String(10))
+   zipcode = db.Column(db.String(10))
+
+   def __init__(self, firstname, lastname, dateofbirth, zipcode):
+      self.firstname = firstname
+      self.lastname = lastname
+      self.dateofbirth = dateofbirth
+      self.zipcode = zipcode
